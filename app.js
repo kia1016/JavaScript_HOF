490fb67679078c314e3e08cd90c087e84343ec0d
kia mangrum <kia1016@gmail.com>
2022-01-16 17:26:31 -0500

    HOF Exercise

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+function plus(number) {
+    return (plusNumber) => (plusNumber += number);
+}
+plus15 = plus(15);
+console.log(plus15(10));
